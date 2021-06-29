7ee66c751c89e397c5c16b8fd87ed4b8bd16ea14
Diego Mucheniski <mucheniski@gmail.com>
2021-06-29 18:01:51 -0300

    Expressions

---
+++ module1/Expressions.py
@@ -0,0 +1,17 @@
+#  MATHEMATICAL OPERATORS
+
+# The numbers are Operands
+# The plus signal are Operators (+ / * - //)
+# + plus
+# - minus
+# * multipli
+# / division
+# // integer division
+10 + 20 + 30
+
+
+# VARIABLES
+my_variable=1
+my_variable:1
+x = 2 * 2
+x = x / 2 
