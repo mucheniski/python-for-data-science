145d792a4d9aac278eebfa65e469f30f461b92df
Diego Mucheniski <mucheniski@gmail.com>
2021-06-29 17:50:10 -0300

    Types and Cast

---
+++ module1/Types.py
@@ -0,0 +1,27 @@
+int typeInt = 11
+float typeFloat = 21.123
+str typeString = "Is this a String"
+
+# CAST
+# float can be convert to int
+# float(2):2.0
+
+# if you cast a float into a int number you will lose information
+# int(1.1):1
+
+# if the string contains integer values, you can cast to int
+# int('1'):1
+
+# If you convert a string with contains non integer values, you get a error
+# int('A'):error
+
+# You can convert a int or float to String
+# str(1):'1'
+# str(1.3):'1.3'
+
+# a boolean contains two values True or False
+# type(True):bool
+# int(True):1
+# int(False):0
+# bool(1):True
+# bool(0):False
