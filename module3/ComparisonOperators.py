647c2bde358506e6f9475d31571c42803f49479b
Diego Mucheniski <mucheniski@gmail.com>
2021-07-01 11:53:40 -0300

    Comparison Operators

---
+++ module3/ComparisonOperators.py
@@ -0,0 +1,23 @@
+# Comparison operators results a boolean value
+# == equal
+# > greather then
+# < lower than
+# >= grether or equal than
+# <= lower or equal than
+# != different
+
+a = 6
+print(a == 7)
+print(a == 6)
+
+# Strings can be copared to
+"Diego" == "Bruna"
+
+age = 17
+if (age > 18):
+    print("you can enter")
+elif (age == 18)
+    print("Goo see Pink Floyd")
+else:
+    print("goo see Meat Loaf")
+print("Move on")
